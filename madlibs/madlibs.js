a57a6e601153c147c0e299874834802c71cc3e6b
Eduard <edwardintoronto@gmail.com>
2015-07-13 15:31:49 -0500

    Update madlibs.js

--- madlibs/madlibs.js
+++ madlibs/madlibs.js
@@ -13,31 +13,7 @@ function replaceW()
   //alert(locationUri);
 
   window.location.href = locationUri+"&oWordPhp="+oWord +"&nWordPhp="+nWord; 
-	
 
-/*
-var xmlhttp;
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-		alert("Success");
-    }
-  }
-  
-  
-xmlhttp.open("GET","http://www.deltadigital.ca/kayoko/wp-content/plugins/madlibs/madlibs.php?oWordPhp="+oWord+"&nWordPhp="+nWord,true);
-xmlhttp.send();
-*/
-  //alert("just after send");
 	
 }
 
