ed79e741f6748950a16a971a735c2a3c583aba22
Eduard <edwardintoronto@gmail.com>
2015-07-13 15:36:57 -0500

    Update madlibs.js

--- madlibs/madlibs.js
+++ madlibs/madlibs.js
@@ -1,29 +1,36 @@
 function replaceW()
 {
+	//get the original word i.e. a word from the text
 	var oWord = document.getElementById("originalWord").value;
+	
+	//get the new word, i.e. typed by the user
 	var nWord = document.getElementById("newWord").value;
 	
 	//alert(oWord);
 	//alert(nWord);
-
-
-  var lastSlash = (window.location.href).lastIndexOf("/");
-  var locationUri = (window.location.href).substring(0,lastSlash) + "?";
+  	//create a url with new word and old word passed in it after ?
+  	//so that they can be extracted by the php function
+        var lastSlash = (window.location.href).lastIndexOf("/");
+        var locationUri = (window.location.href).substring(0,lastSlash) + "?";
   
-  //alert(locationUri);
+        //alert(locationUri);
 
-  window.location.href = locationUri+"&oWordPhp="+oWord +"&nWordPhp="+nWord; 
+        window.location.href = locationUri+"&oWordPhp="+oWord +"&nWordPhp="+nWord; 
 
 	
 }
 
+//Reset button on the widget, reloads the page
 function resetUrl()
 {
+	//find the index of ?
 	var n = window.location.href.indexOf("?"); 
 	//alert(n);
 	
+	//get the url without all arguments after ?
 	var res = window.location.href.substring(0, n); 
 	
 	//alert(res);
+	//go to url
 	window.location.href = res;
 }
